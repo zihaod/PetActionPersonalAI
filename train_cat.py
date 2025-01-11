66f7085f1e0320d5aa545e34f1f9be4992a9570b
zihaod <65971377+zihaod@users.noreply.github.com>
2025-01-11 07:43:36 -0500

    Update train_cat.py

--- train_cat.py
+++ train_cat.py
@@ -28,21 +28,22 @@ if __name__ == '__main__':
     date = '20250109'
 
     # For dataset
-    cat_data = []
+    base_data = []
     fpaths_dict = {}
+    base_data_dir = '/content/drive/MyDrive/pet_project/pet_action/6月10日json合集'
     data_dir = '/content/drive/MyDrive/pet_project/pet_action/personal_AI_test_data/20250109数据标注/'
     
     # Model weight ckpt
     pretrained_model_path = '/content/drive/MyDrive/pet_project/pet_action/cat_models/cat_hidden_72_seq_20_scaledown_100_accdiff_20240622.pt'
     
-    ##############################
+    #######################################
 
     # Load data from main database
-    for fname in os.listdir('/content/drive/MyDrive/pet_project/pet_action/6月10日json合集'):
+    for fname in os.listdir(base_data_dir):
         if fname.startswith('猫') and fname.endswith('.json'):
-            fpath = '/content/drive/MyDrive/pet_project/pet_action/6月10日json合集/' + fname
+            fpath = base_data_dir + fname
             data = read_json_data(fpath)
-            cat_data.append(data)
+            base_data.append(data)
             
 
     # Group personal data files by identities
@@ -55,9 +56,12 @@ if __name__ == '__main__':
                 fpaths_dict[k].append(p)
     
     print(fpaths_dict)
+
+    
+    #######################################
     
     # Load base dataset
-    dataset = ActionDataset(cat_data, actions, seq_len=20, step_size=1, diff_mode="acc", augment=None)
+    dataset = ActionDataset(base_data, actions, seq_len=20, step_size=1, diff_mode="acc", augment=None)
     data_loader = DataLoader(dataset, batch_size=batch_size, shuffle=False)
     
     # Get Base data labels and split based on label
